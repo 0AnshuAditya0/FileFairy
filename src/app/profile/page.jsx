bfad51e5ff4283fadcea8bff27f84a1bb50849fb
Parth <parthbidari13@gmail.com>
2024-11-03 13:17:19 +0530

    minor changes in the page UI

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center min-h-screen p-6 sm:p-8 ">
+    <div className="w-full max-w-3xl p-8 space-y-6 flex justify-center items-center rounded-lg">
+      profile..
+    </div>
+  </div>
   )
 }
 
